aed4e096e2714317f1cea7d3464aaced3a5d2eaa
areverdy <alec.rev1234@gmail.com>
2023-09-22 16:55:06 +0200

    mise à jour du constructor de la classe personnage et debut creation personnage

--- src/Vikings le jeu/Personnages/Personnages.ts
+++ src/Vikings le jeu/Personnages/Personnages.ts
@@ -5,113 +5,34 @@ import { Metier } from "../Metiers"
 export class Personnage {
 
     public nom: string
-    public sante: number
-    private _santeInitiale: number
+    private _santeInitiale: number = 50
     private _metier: Metier
-
     private _force: number = 10
-    private _arme: Arme
-    public get arme(): Arme {
-        return this._arme
-    }
-    public set arme(value: Arme) {
-        this._arme = value
-    }
-    private _defense: number
-    public get defense(): number {
-        return this._defense
-    }
-    public set defense(value: number) {
-        this._defense = value
-    }
     private _niveau: number = 1
-    public get niveau(): number {
-        return this._niveau
-    }
-    public set niveau(value: number) {
-        this._niveau = value
-    }
     private _nbreptexperience: number = 0
-    public get nbreptexperience(): number {
-        return this._nbreptexperience
-    }
-    public set nbreptexperience(value: number) {
-        this._nbreptexperience = value
-    }
-    private _type: number = 0
-    public get type(): number {
-        return this._type
-    }
-    public set type(value: number) {
-        this._type = value
-    }
-    private _vitesse: number
-    public get vitesse(): number {
-        return this._vitesse
-    }
-    public set vitesse(value: number) {
-        this._vitesse = value
-    }
+    private _vitesse: number = 10
     private _intelligence: number = 10
-    public get intelligence(): number {
-        return this._intelligence
-    }
-    public set intelligence(value: number) {
-        this._intelligence = value
-    }
-    private _mana: number = 50
-    public get mana(): number {
-        return this._mana
-    }
-    public set mana(value: number) {
-        this._mana = value
-    }
+    private _mana: number = 50 
     private _pctchance: number = 0.02
-    public get pctchance(): number {
-        return this._pctchance
-    }
-    public set pctchance(value: number) {
-        this._pctchance = value
-    }
-    
-
-
+     
     constructor(
         
         nom: string,
         metier : Metier,
-        sante: number,
-        santeInitiale: number,
-        force: number,
-        arme: Arme,
-        defense: number,
-        niveau: number,
-        nbreptexperience: number,
-        type: number,
-        vitesse: number,
-        intelligence: number,
-        mana: number,
-        pctchance: number,
 
     ) {
         this.nom = nom
         this._metier = metier
-        this.sante = sante
-        this._santeInitiale = santeInitiale + this.metier.santebonus
-        this._force = force + this.metier.forcebonus
-        this._arme = arme
-        this._defense = defense
-        this._niveau = niveau
-        this._nbreptexperience = nbreptexperience
-        this._type = type
-        this._vitesse = vitesse + this.metier.vitessebonus
-        this._intelligence = intelligence + this.metier.intelligencebonus
-        this._mana = mana + this.metier.manabonus
-        this._pctchance = pctchance + this.metier.pctpointdechancebonus
+        this._santeInitiale = this.santeInitiale + this.metier.santebonus
+        this._force = this.force + this.metier.forcebonus
+        this._niveau = this.niveau
+        this._nbreptexperience = this.nbreptexperience
+        this._vitesse = this.vitesse + this.metier.vitessebonus
+        this._intelligence = this.intelligence + this.metier.intelligencebonus
+        this._mana = this.mana + this.metier.manabonus
+        this._pctchance = this.pctchance + this.metier.pctpointdechancebonus
 
     }
-
-
     public get santeInitiale(): number {
         return this._santeInitiale
     }
@@ -124,5 +45,47 @@ export class Personnage {
     }
     public set metier(value: Metier) {
         this._metier = value
+    }
+    public get niveau(): number {
+        return this._niveau
+    }
+    public set niveau(value: number) {
+        this._niveau = value
+    }
+    public get nbreptexperience(): number {
+        return this._nbreptexperience
+    }
+    public set nbreptexperience(value: number) {
+        this._nbreptexperience = value
+    }
+    public get vitesse(): number {
+        return this._vitesse
+    }
+    public set vitesse(value: number) {
+        this._vitesse = value
+    }
+    public get intelligence(): number {
+        return this._intelligence
+    }
+    public set intelligence(value: number) {
+        this._intelligence = value
+    }
+    public get mana(): number {
+        return this._mana
+    }
+    public set mana(value: number) {
+        this._mana = value
+    }
+    public get pctchance(): number {
+        return this._pctchance
+    }
+    public set pctchance(value: number) {
+        this._pctchance = value
+    }
+    public get force(): number {
+        return this._force
+    }
+    public set force(value: number) {
+        this._force = value
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import { Viking } from "./Vikings le jeu/Viking"
-import { Arme } from "./Vikings le jeu/Arme"
-import { Combat } from "./Vikings le jeu/Combat"
+// import { Viking } from "./Vikings le jeu/Viking"
+// import { Arme } from "./Vikings le jeu/Arme"
+// import { Combat } from "./Vikings le jeu/Combat"
 
-const armeG = new Arme("Seax",8)
-const armeH = new Arme("Hache",9)
+import { Newviking } from "./Vikings le jeu/Personnages/NewVikings";
+import { Personnage } from "./Vikings le jeu/Personnages/Personnages";
 
-const Gudrun = new Viking('Gudrun', 98, 8,armeG)
-console.log(Gudrun)
+// const armeG = new Arme("Seax",8)
+// const armeH = new Arme("Hache",9)
 
-const Holda = new Viking("Holda", 85, 9, armeH)
-console.log(Holda)
+// const Gudrun = new Viking('Gudrun', 98, 8,armeG)
+// console.log(Gudrun)
 
-const combat = new Combat(Gudrun, Holda)
-combat.combattre()
-combat.affichervainqueur()
+// const Holda = new Viking("Holda", 85, 9, armeH)
+// console.log(Holda)
+
+// const combat = new Combat(Gudrun, Holda)
+// combat.combattre()
+// combat.affichervainqueur()
+
+const Ragnar = new Personnage("Ragnar", Newviking)
